7deeb30202219b84d50c378b928c15e1bc0ff842
khfy7wpr5p-maker <onderozudogru@gmail.com>
2026-08-21 20:01:03 +0300

    Stage 5-B3a: distinguish exact replay from state corruption

--- services/homr-service/src/scoremosaic_homr/__init__.py
+++ services/homr-service/src/scoremosaic_homr/__init__.py
@@ -5,6 +5,11 @@ from .source_delivery_hardening import activate as _activate_source_delivery_har
 _activate_source_delivery_hardening()
 del _activate_source_delivery_hardening
 
+from .authenticated_execution_trigger_hardening import activate as _activate_execution_trigger_hardening
+
+_activate_execution_trigger_hardening()
+del _activate_execution_trigger_hardening
+
 __all__ = ["__version__"]
 
 __version__ = "0.2.0"
